f0d5b16141dd02db5203a03c412c27400b34da54
Bird Egop <sampletext32@bk.ru>
2020-12-09 21:04:42 +0300

    Add project

---
+++ Andreev_4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Andreev_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //В зависимости от языка системы дробные числа с точкой не конвертируются из строк
+            //поэтому точки вручную заменяем на запятые
+            //.Replace(".", ",")
+
+            Console.WriteLine("Введите x: ");
+            double x = double.Parse(Console.ReadLine().Replace(".", ","));
+            Console.WriteLine("Введите y: ");
+            double y = double.Parse(Console.ReadLine().Replace(".", ","));
+            Console.WriteLine("Введите n: ");
+            double n = double.Parse(Console.ReadLine().Replace(".", ","));
+
+            int sign = 1;
+
+            double yfib1 = y;
+            double yfib2 = y;
+
+            double fact_i = 3;
+            double fact = 2;
+
+            double tgy_i = Math.Tan(y);
+            double tgy = tgy_i;
+
+            double x_pow = x;
+
+            double A = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                A += sign * yfib1 * yfib2 * Math.Sin(fact) / (tgy + Math.Pow(x_pow, i + 1));
+                Console.WriteLine($"A(n={i}) = {A:F8}");
+                sign *= -1;
+
+                double yfib = yfib1 * yfib2;
+                yfib1 = yfib2;
+                yfib2 = yfib;
+
+                fact *= fact_i;
+
+                tgy *= tgy_i;
+                x_pow *= x;
+            }
+        }
+    }
+}
